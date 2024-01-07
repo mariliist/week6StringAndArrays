f0262ace542c2665f9e4a7d6855275179665f631
Mariliis Toon <mariliistoon@gmail.com>
2024-01-07 15:23:29 +0200

    first

---
+++ Hindeline/Program.cs
@@ -0,0 +1,18 @@
+﻿//string[] colors = { "red", "green", "blue", "pink", "yellow" };
+
+//for (int i = colors.Length - 1; i >= 0; i--)
+//{
+//    Console.WriteLine($"{i}. {colors[i]}");
+//}
+
+string hello = "Hello, world!";
+int counter = 0;
+
+for (int i = 0; i < hello.Length; i++)
+{
+    if (hello[i] == 'l')
+    {
+        counter++;
+    }
+}
+Console.WriteLine($"There are {counter} l's in the string \"{hello}\"");
--- week6StringAndArrays/Program.cs
+++ week6StringAndArrays/Program.cs
@@ -1,3 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string hello = " cheers to the world, world! I is here?";
+
+int stringLength = hello.Length;
+
+//string trimmedString = hello.Trim();
+
+hello = hello.Trim();
+char firstLetter = hello[0];
+
+Console.WriteLine(firstLetter);
+
+hello = hello.Replace(hello[0], char.ToUpper(hello[0])).Replace('?', '!');
+
+firstLetter = hello[0];
+
+Console.WriteLine(firstLetter);
+
+Console.WriteLine(hello);
+
+//int wordCounter = 0;
+//for(int i = 0; i < hello.Length; i++)
+//{
+//    if (char.IsWhiteSpace(hello[i]))
+//    {
+//        wordCounter++;
+//    }
+//}
+
+//Console.WriteLine($" Word count in {hello}: {wordCounter + 1} words.");
 
